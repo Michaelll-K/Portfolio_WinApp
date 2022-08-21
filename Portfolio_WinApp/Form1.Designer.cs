6be594fe0542e36a36d8f7cf9dff1053b0f8570c
Michał Kordos <michal.kordos8@gmail.com>
2022-08-21 18:37:45 +0200

    Add project files.

---
+++ Portfolio_WinApp/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Portfolio_WinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbMain = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbMain
+            // 
+            this.gbMain.Controls.Add(this.label1);
+            this.gbMain.Controls.Add(this.checkBox1);
+            this.gbMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gbMain.Location = new System.Drawing.Point(0, 0);
+            this.gbMain.Name = "gbMain";
+            this.gbMain.Size = new System.Drawing.Size(800, 450);
+            this.gbMain.TabIndex = 0;
+            this.gbMain.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(49, 85);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(228, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gbMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gbMain.ResumeLayout(false);
+            this.gbMain.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbMain;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
