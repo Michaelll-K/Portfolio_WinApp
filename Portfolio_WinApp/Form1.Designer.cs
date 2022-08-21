e916cdc2e4bf0607b915b7e5ba124394faca02c2
Michał Kordos <michal.kordos8@gmail.com>
2022-08-21 19:30:08 +0200

    Style of Form

--- Portfolio_WinApp/Form1.Designer.cs
+++ Portfolio_WinApp/Form1.Designer.cs
@@ -28,52 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.gbMain = new System.Windows.Forms.GroupBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.gbMain.SuspendLayout();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.SuspendLayout();
             // 
             // gbMain
             // 
-            this.gbMain.Controls.Add(this.label1);
-            this.gbMain.Controls.Add(this.checkBox1);
             this.gbMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.gbMain.Location = new System.Drawing.Point(0, 0);
             this.gbMain.Name = "gbMain";
-            this.gbMain.Size = new System.Drawing.Size(800, 450);
+            this.gbMain.Size = new System.Drawing.Size(1101, 573);
             this.gbMain.TabIndex = 0;
             this.gbMain.TabStop = false;
             // 
-            // checkBox1
+            // contextMenuStrip1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(49, 85);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(228, 112);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1101, 573);
             this.Controls.Add(this.gbMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.gbMain.ResumeLayout(false);
-            this.gbMain.PerformLayout();
+            this.Text = "Michał Kordos - Portfolio_WinApp";
             this.ResumeLayout(false);
 
         }
@@ -81,8 +64,7 @@
         #endregion
 
         private System.Windows.Forms.GroupBox gbMain;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
     }
 }
 
