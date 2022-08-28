c58bbe41ba531c8fa316eec810ca13f3991d63fd
Michał Kordos <michal.kordos8@gmail.com>
2022-08-28 21:49:52 +0200

    Basic Account panel design

--- Portfolio_WinApp/Controls/AccountControl.Designer.cs
+++ Portfolio_WinApp/Controls/AccountControl.Designer.cs
@@ -30,29 +30,36 @@
         {
             this.pbx = new System.Windows.Forms.PictureBox();
             this.panelUser = new System.Windows.Forms.Panel();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.btnSkills = new System.Windows.Forms.Button();
-            this.btnAboutMe = new System.Windows.Forms.Button();
-            this.btnExp = new System.Windows.Forms.Button();
-            this.btnEdu = new System.Windows.Forms.Button();
             this.btnLinks = new System.Windows.Forms.Button();
             this.btnContact = new System.Windows.Forms.Button();
             this.btnRef = new System.Windows.Forms.Button();
+            this.btnExp = new System.Windows.Forms.Button();
+            this.btnEdu = new System.Windows.Forms.Button();
+            this.btnAboutMe = new System.Windows.Forms.Button();
+            this.btnSkills = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.lblMichal = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbx)).BeginInit();
             this.panelUser.SuspendLayout();
-            this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // pbx
             // 
             this.pbx.Image = global::Portfolio_WinApp.Properties.Resources.user;
-            this.pbx.Location = new System.Drawing.Point(39, 148);
+            this.pbx.Location = new System.Drawing.Point(16, 88);
             this.pbx.Name = "pbx";
-            this.pbx.Size = new System.Drawing.Size(240, 240);
-            this.pbx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx.Size = new System.Drawing.Size(293, 240);
+            this.pbx.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbx.TabIndex = 0;
             this.pbx.TabStop = false;
             // 
@@ -65,16 +72,6 @@
             this.panelUser.Name = "panelUser";
             this.panelUser.Size = new System.Drawing.Size(786, 542);
             this.panelUser.TabIndex = 1;
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.Lavender;
-            this.panel1.Controls.Add(this.lblMichal);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(786, 100);
-            this.panel1.TabIndex = 0;
             // 
             // panel2
             // 
@@ -91,66 +88,6 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(786, 100);
             this.panel2.TabIndex = 1;
-            // 
-            // btnSkills
-            // 
-            this.btnSkills.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.btnSkills.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.btnSkills.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSkills.Font = new System.Drawing.Font("Malgun Gothic", 12F);
-            this.btnSkills.Location = new System.Drawing.Point(125, 0);
-            this.btnSkills.Margin = new System.Windows.Forms.Padding(3, 3, 15, 3);
-            this.btnSkills.Name = "btnSkills";
-            this.btnSkills.Size = new System.Drawing.Size(101, 100);
-            this.btnSkills.TabIndex = 0;
-            this.btnSkills.Text = "Skills and Abilities";
-            this.btnSkills.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btnSkills.UseVisualStyleBackColor = false;
-            // 
-            // btnAboutMe
-            // 
-            this.btnAboutMe.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.btnAboutMe.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.btnAboutMe.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAboutMe.Font = new System.Drawing.Font("Malgun Gothic", 12F);
-            this.btnAboutMe.Location = new System.Drawing.Point(17, 0);
-            this.btnAboutMe.Margin = new System.Windows.Forms.Padding(3, 3, 15, 3);
-            this.btnAboutMe.Name = "btnAboutMe";
-            this.btnAboutMe.Size = new System.Drawing.Size(101, 100);
-            this.btnAboutMe.TabIndex = 1;
-            this.btnAboutMe.Text = "About Me";
-            this.btnAboutMe.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btnAboutMe.UseVisualStyleBackColor = false;
-            // 
-            // btnExp
-            // 
-            this.btnExp.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.btnExp.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.btnExp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnExp.Font = new System.Drawing.Font("Malgun Gothic", 12F);
-            this.btnExp.Location = new System.Drawing.Point(234, 0);
-            this.btnExp.Margin = new System.Windows.Forms.Padding(3, 3, 15, 3);
-            this.btnExp.Name = "btnExp";
-            this.btnExp.Size = new System.Drawing.Size(101, 100);
-            this.btnExp.TabIndex = 3;
-            this.btnExp.Text = "Experience";
-            this.btnExp.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btnExp.UseVisualStyleBackColor = false;
-            // 
-            // btnEdu
-            // 
-            this.btnEdu.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.btnEdu.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.btnEdu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnEdu.Font = new System.Drawing.Font("Malgun Gothic", 12F);
-            this.btnEdu.Location = new System.Drawing.Point(343, 0);
-            this.btnEdu.Margin = new System.Windows.Forms.Padding(3, 3, 15, 3);
-            this.btnEdu.Name = "btnEdu";
-            this.btnEdu.Size = new System.Drawing.Size(101, 100);
-            this.btnEdu.TabIndex = 2;
-            this.btnEdu.Text = "Education";
-            this.btnEdu.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.btnEdu.UseVisualStyleBackColor = false;
             // 
             // btnLinks
             // 
@@ -197,6 +134,76 @@
             this.btnRef.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnRef.UseVisualStyleBackColor = false;
             // 
+            // btnExp
+            // 
+            this.btnExp.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnExp.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnExp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExp.Font = new System.Drawing.Font("Malgun Gothic", 12F);
+            this.btnExp.Location = new System.Drawing.Point(234, 0);
+            this.btnExp.Margin = new System.Windows.Forms.Padding(3, 3, 15, 3);
+            this.btnExp.Name = "btnExp";
+            this.btnExp.Size = new System.Drawing.Size(101, 100);
+            this.btnExp.TabIndex = 3;
+            this.btnExp.Text = "Experience";
+            this.btnExp.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnExp.UseVisualStyleBackColor = false;
+            // 
+            // btnEdu
+            // 
+            this.btnEdu.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnEdu.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnEdu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEdu.Font = new System.Drawing.Font("Malgun Gothic", 12F);
+            this.btnEdu.Location = new System.Drawing.Point(343, 0);
+            this.btnEdu.Margin = new System.Windows.Forms.Padding(3, 3, 15, 3);
+            this.btnEdu.Name = "btnEdu";
+            this.btnEdu.Size = new System.Drawing.Size(101, 100);
+            this.btnEdu.TabIndex = 2;
+            this.btnEdu.Text = "Education";
+            this.btnEdu.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnEdu.UseVisualStyleBackColor = false;
+            // 
+            // btnAboutMe
+            // 
+            this.btnAboutMe.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnAboutMe.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnAboutMe.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAboutMe.Font = new System.Drawing.Font("Malgun Gothic", 12F);
+            this.btnAboutMe.Location = new System.Drawing.Point(17, 0);
+            this.btnAboutMe.Margin = new System.Windows.Forms.Padding(3, 3, 15, 3);
+            this.btnAboutMe.Name = "btnAboutMe";
+            this.btnAboutMe.Size = new System.Drawing.Size(101, 100);
+            this.btnAboutMe.TabIndex = 1;
+            this.btnAboutMe.Text = "About Me";
+            this.btnAboutMe.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnAboutMe.UseVisualStyleBackColor = false;
+            // 
+            // btnSkills
+            // 
+            this.btnSkills.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btnSkills.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnSkills.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSkills.Font = new System.Drawing.Font("Malgun Gothic", 12F);
+            this.btnSkills.Location = new System.Drawing.Point(125, 0);
+            this.btnSkills.Margin = new System.Windows.Forms.Padding(3, 3, 15, 3);
+            this.btnSkills.Name = "btnSkills";
+            this.btnSkills.Size = new System.Drawing.Size(101, 100);
+            this.btnSkills.TabIndex = 0;
+            this.btnSkills.Text = "Skills and Abilities";
+            this.btnSkills.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnSkills.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Lavender;
+            this.panel1.Controls.Add(this.lblMichal);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(786, 100);
+            this.panel1.TabIndex = 0;
+            // 
             // lblMichal
             // 
             this.lblMichal.Anchor = System.Windows.Forms.AnchorStyles.None;
@@ -211,19 +218,78 @@
             this.lblMichal.Text = "Michał Kordos\r\nbackend developer";
             this.lblMichal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Transparent;
+            this.panel3.Controls.Add(this.lblTitle);
+            this.panel3.Controls.Add(this.panel5);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Controls.Add(this.pbx);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(315, 542);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(315, 43);
+            this.panel4.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel5.Location = new System.Drawing.Point(0, 43);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(10, 499);
+            this.panel5.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Malgun Gothic", 20F);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 37);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Account Panel: ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblTitle.Location = new System.Drawing.Point(10, 46);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(305, 23);
+            this.lblTitle.TabIndex = 3;
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
             // AccountControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.panelUser);
-            this.Controls.Add(this.pbx);
             this.Name = "AccountControl";
             this.Size = new System.Drawing.Size(1101, 542);
             ((System.ComponentModel.ISupportInitialize)(this.pbx)).EndInit();
             this.panelUser.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -242,5 +308,10 @@
         private System.Windows.Forms.Button btnEdu;
         private System.Windows.Forms.Button btnAboutMe;
         private System.Windows.Forms.Label lblMichal;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label label1;
     }
 }
